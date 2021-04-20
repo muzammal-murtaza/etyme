21069638e112df8ca5b980f025252131c216c85f
muzammal <muzammalmurtaza13@gmail.com>
2021-04-20 07:42:34 -0700

    fixx

--- config/initializers/delay_jobs.rb
+++ config/initializers/delay_jobs.rb
@@ -8,4 +8,4 @@ Delayed::Worker.queue_attributes = {
 
 Delayed::Worker.default_queue_name = 'default_q'
 Delayed::Worker.destroy_failed_jobs = false
-# Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
